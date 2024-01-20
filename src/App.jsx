110925d0f3ce56ac7cbe6f1f0285dfd7c3175333
Prasenjit Aluni <prasenjit.aluni@codeclouds.com>
2024-01-20 19:13:37 +0530

    HOC : Higher Order Component

--- src/App.jsx
+++ src/App.jsx
@@ -3,33 +3,29 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
+let cnt = parseInt(10*Math.random());
 
+const CompA = () => {
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h3>-: CompA ({cnt}) :-</h3>
     </>
   )
 }
 
-export default App
+function CompB() {
+  return (
+    <>
+      <h3>-: CompB ({cnt}) :-</h3>
+    </>
+  )
+}
+
+function HOC(A, B){
+  return cnt > 5 ? A : B;
+}
+
+const Pqr = HOC(CompA, CompB);
+
+export default Pqr;
+
